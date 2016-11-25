e0de77ec66dce15e257865c01ba7de96b9f3483e
Alberto M Esquivias <alberto.m.esquivias@gmail.com>
2016-11-25 14:26:55 -0800

    [server] Inital Server File

---
+++ src/server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const moment = require('moment');
+
+const app = express();
+
+app.set('PORT', process.env.PORT || 2222);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(express.static('../../www'));
+
+app.listen(app.get('PORT'), () => {
+	console.log(`[${moment().format('hh:mm:ss')}]Express Server listening on port`, app.get('PORT'));
+});
+
+module.exports = app;
