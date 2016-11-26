73dbd60c153d8b1c69419b2fb6c8b976825780f5
Cristian Esquivias <cristian.esquivias@gmail.com>
2016-11-25 19:16:02 -0800

    [server] Adds /santa endpoint. Can GET or POST

---
+++ server/README.md
@@ -0,0 +1,7 @@
+# Endpoints
+## /santa
+Resource for specific Secret Santa gift exchanges
+### GET
+Returns all Secret Santa exchanges the user is involved in.
+### POST
+Creates a new Secret Santa exchange. The new object is returned.
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const moment = require('moment');
 
+const db = require('./db.js');
+
 const app = express();
 
 app.set('PORT', process.env.PORT || 2222);
@@ -11,7 +13,24 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-	res.end('hello world'); 
+	res.end('Hello, Santa!'); 
+});
+
+app.get('/santa', (req, res) => {
+    db.Santa.findAll({limit: 10, order: 'updated_at DESC'}).then((santas) => {
+        res.json(santas);
+    });
+});
+
+app.post('/santa', (req, res) => {
+    db.Santa.findOrCreate({where: {name: req.body.name}}).spread((santa, isCreated) => {
+        if (isCreated) {
+            res.json(santa);
+        } else {
+            res.status(409);
+            res.send('Santa already exists');
+        }
+    });
 });
 
 app.listen(app.get('PORT'), () => {
