f654a7a04dc60e825c0bc7b32656117d08fb7135
Alberto M Esquivias <alberto.m.esquivias@gmail.com>
2016-11-26 18:05:59 -0800

    [feature] Adds and removes Secret Santa input fields

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -3,13 +3,27 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	numSantas = 4;
+	totalSantas = ['Santa 1', 'Santa 2', 'Santa 3', 'Santa 4'];
 
-  }
+	addSanta() {
+		this.totalSantas.push(`Santa ${this.totalSantas.length + 1}`);
+	}
+
+	removeSanta() {
+		if (this.totalSantas.length) {
+			this.totalSantas.pop();
+		}
+	}
+
+	constructor(public navCtrl: NavController) {
+
+	}
 
 }
